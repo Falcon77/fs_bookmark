6f492fa97ce28d0679ceffd556e64f123d1cd144
Fermin Suero <fsuero@yahoo.com>
2012-08-20 22:46:28 -0400

    update 08202012 pm

--- application/views/home/default.php
+++ application/views/home/default.php
@@ -1,13 +1,10 @@
 
 
-<h1>Show this page when not logged in.</h1>
+<h1>Show this page when user is logged in.</h1>
 
-<?php echo form_open(''); ?><br />
-Login:<?php echo form_input('login', ''); ?><br />
-Password:<?php echo form_input('password', ''); ?><br />
-Email:<?php echo form_input('email', ''); ?><br />
-<?php echo form_submit('submit', 'submit'); ?><br />
-<?php echo form_close(); ?>
+<?php echo $user_id; ?><br/>
+<?php echo $username; ?><br/>
+Testing
 
 <br /><br />
 
---
+++ randomnumber.php
@@ -0,0 +1,4 @@
+
+<?php
+//random number between 0 and 10 (inclusive)
+echo mt_rand(2468549874, 9921684168);
