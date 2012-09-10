9e0083a2a889d7b98e8c48950411c1bc86abf4d0
Fermin Suero <fsuero@yahoo.com>
2012-09-10 16:10:44 -0400

    changes to header

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
     
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.css">
     <style>
         body {
             padding-top: 60px;
@@ -34,45 +34,45 @@
     <script type="text/javascript" src="<?php echo base_url(); ?>public/js/custom.js"></script>
     
 </head>
-<body>
+<body class="">
     <!--[if lt IE 7]>
         <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
     <![endif]-->
-<div class="navbar navbar-inverse navbar-fixed-top">Test
-</div>
 <!-- start:Wrap -->
 <div id="wrap">
+    
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#">StartHo.me</a>
+            <div class="nav-collapse collapse">
+                <?php echo form_open('login/run',array('id' => 'login_form', 'class' => 'navbar-form pull-right')); ?>
+                    <?php echo form_input(array('name' => 'email', 'id' => 'email', 'class' => 'span2', 'tabindex' => '1', 'placeholder' => 'Email')); ?>
+                    <?php echo form_password(array('name' => 'pass', 'id' => 'pass', 'class' => 'span2', 'tabindex' => '2', 'placeholder' => 'Password')); ?>
+                    <?php echo form_submit(array('class' => 'btn', 'value' => 'Log in', 'id' => 'ulheader', 'tabindex' => '4')); ?>
+                    <div class="controls controls-row">
+                        <label class="checkbox">
+                            <input type="checkbox"> Keep me logged in &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="#">Forgot your password?</a>
+                        </label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+    
+
      
 <!-- start:Header -->
-<div id="header">
-    <!-- login form -->
-    <div align="right">
-        <?php echo form_open('login/run',array('id' => 'login_form', 'class' => 'myFormClass')); ?>
-        <table cellspacing="0" border="0">
-            <tr>
-                <td class="html7magic"><?php echo form_label('Email', 'email'); ?></td>
-                <td class="html7magic"><?php echo form_label('Password', 'pass'); ?></td>
-            </tr>
-            <tr>
-                <td><?php echo form_input(array('name' => 'email', 'id' => 'email', 'class' => 'inputtext', 'tabindex' => '1')); ?></td>
-                <td><?php echo form_password(array('name' => 'pass', 'id' => 'pass', 'class' => 'inputtext', 'tabindex' => '2')); ?></td>
-                <td><?php echo form_label(form_submit(array('value' => 'Log in', 'id' => 'ulheader', 'tabindex' => '4')),'ulheader', array('class' => 'uibutton', 'id' => 'loginbutton')); ?></td>
-            </tr>
-            <tr>
-                <td class="login_form_label_field"><div class="uiInputLabel clearfix">
-                        <?php echo form_checkbox(array('id' => 'persist_box', 'name' => 'persistent', 'value' => '1', 'checked' => 'true', 'class' => 'uiCheckbox')); ?>
-                        <?php echo form_label('Keep me logged in', 'persist_box'); ?></div>
-                        <?Php echo form_hidden('default_persistent', '1'); ?>
-                </td>
-                <td class="login_form_label_field">
-                    <a rel="nofollow" href="">Forgot your password?</a>
-                </td>
-            </tr>
-        </table>
-        <?php echo form_close(); ?>
-    </div>
-    <!-- end login form -->
-</div>
+<!div id="header">
+
+<!/div>
 
 <!-- start:Content -->
 <div id="content">
